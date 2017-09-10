82ff97786e898933dc40f8e258b93c428553a5bc
Angelito Sardez, Jr <jojosardez@gmail.com>
2017-09-10 15:13:44 +0800

    Implemented parameterized routes and route Guards.

--- APM/src/app/products/product-detail.component.ts
+++ APM/src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute } from '@angular/router';
+import { Router } from '@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,9 +10,26 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
     pageTitle: string = "Prdoduct Detail";
     product: IProduct;
-  constructor() { }
 
-  ngOnInit() {
+  constructor(private _route: ActivatedRoute,
+            private _router: Router) { 
   }
 
+  ngOnInit() {
+    let id = +this._route.snapshot.paramMap.get("id");
+    this.product = {
+      "productId": id,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.85,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+  }
+
+  onBack(): void {
+    this._router.navigate(["/products"]);
+  }
 }
